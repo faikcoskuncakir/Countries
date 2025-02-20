0f4524cd2dee2f81ea4ca7783125855f4711e110
Faik Coşkun Çakır <fcoskuncakir@hotmail.com>
2025-02-20 17:14:25 +0300

    updated service layer for Get requests to add query parameters

--- Countries/Network Layer/Core/NetworkService.swift
+++ Countries/Network Layer/Core/NetworkService.swift
@@ -12,26 +12,29 @@ class NetworkService {
     static let shared = NetworkService()
     
     /// URLSession has been added for use if desired.
-    func requestWithURLSession<T: Codable>(url: String, method: HTTPMethods, completion: @escaping((Result<T, NetworkErrors>)->Void)) {
-        let session = URLSession.shared
-        if let url = URL(string: url) {
-            var request = URLRequest(url: url)
-            request.httpMethod = method.rawValue
-            request.setValue(ServiceContants.apiKey.rawValue, forHTTPHeaderField: ServiceContants.apiHTTPField.rawValue)
-            
-            let dataTask = session.dataTask(with: request) { (data, response, error) in
-                if let _ = error {
-                    completion(.failure(.generalError))
-                } else if let data = data {
-                    self.handleResponse(data: data) { response in
-                        completion(response)
-                    }
-                } else {
-                    completion(.failure(.invalidURL))
-                }
-            }
-            dataTask.resume()
+    func requestWithURLSession<T: Codable>(url: String, method: HTTPMethods, queryItems: [URLQueryItem]? = nil, completion: @escaping (Result<T, NetworkErrors>) -> Void) {
+        var components = URLComponents(string: url)
+        components?.queryItems = queryItems
+        
+        guard let finalURL = components?.url else {
+            completion(.failure(.invalidURL))
+            return
         }
+        
+        var request = URLRequest(url: finalURL)
+        request.httpMethod = method.rawValue
+        request.setValue(ServiceContants.apiKey.rawValue, forHTTPHeaderField: ServiceContants.apiHTTPField.rawValue)
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let _ = error {
+                completion(.failure(.generalError))
+            } else if let data = data {
+                self.handleResponse(data: data, completion: completion)
+            } else {
+                completion(.failure(.invalidURL))
+            }
+        }
+        dataTask.resume()
     }
     
     private func handleResponse<T: Codable>(data: Data, completion: @escaping((Result<T, NetworkErrors>)->Void)) {
